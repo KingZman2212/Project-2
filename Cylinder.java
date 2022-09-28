fbc381cb0465eb07ac734d27fcfc681d457ab766
KingZman2212 <zac2188946@maricopa.edu>
2022-09-28 14:44:11 -0700

    Initial commit

---
+++ Cylinder.java
@@ -0,0 +1,67 @@
+package com.csc205.project2;
+
+public class Cylinder extends Shape {
+
+    private double height;
+    private double radius;
+
+    public Cylinder() {
+
+        super();
+
+        this.height = 0.0;
+        this.radius = 0.0;
+    }
+
+    public Cylinder (double x, double y)  {
+
+        super();
+
+        this.height = x;
+        this.radius = y;
+    }
+
+    public double getHeight() {
+
+        return height;
+    }
+
+    public double getRadius() {
+
+        return radius;
+    }
+
+    public void setHeight(double height) {
+
+        this.height = height;
+    }
+
+    public void setRadius(double radius) {
+
+        this.radius = radius;
+    }
+
+    @Override
+    public double surfaceArea() {
+
+        return (2 * Math.PI * radius * height) + (2 * Math.PI * Math.pow(radius, 2));
+    }
+
+    @Override
+    public double volume() {
+
+        return Math.PI * Math.pow(radius, 2) * height;
+    }
+
+    @Override
+    public String toString() {
+
+        final StringBuilder sb = new StringBuilder("Cylinder {");
+        sb.append("height = ").append(height);
+        sb.append(", radius = ").append(radius);
+        sb.append(", surface area = ").append(surfaceArea());
+        sb.append(", volume = ").append(volume());
+        sb.append('}');
+        return sb.toString();
+    }
+}
